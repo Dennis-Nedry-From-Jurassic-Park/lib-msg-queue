5c445493cf127d3de95a9e0f926de3658a46ce21
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-03-26 20:30:56 +0300

    debug

--- src/nats/conn.ts
+++ src/nats/conn.ts
@@ -4,11 +4,15 @@ export const sc = StringCodec();
 
 export const nats_client = connect({
     servers: `${process.env.NATS_SERVER}:${process.env.NATS_PORT}`,
-    token: process.env.NATS_TOKEN,
+    token: `${process.env.NATS_TOKEN}`,
     debug: false,
     verbose: true,
     maxPingOut: 5
 });
+
+console.log(process.env.NATS_TOKEN);
+console.log(process.env.NATS_SERVER);
+console.log(process.env.NATS_PORT);
 
 delete process.env.NATS_TOKEN
 delete process.env.NATS_SERVER
