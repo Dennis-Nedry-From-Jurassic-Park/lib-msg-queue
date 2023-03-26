79922f88561b55049af8db5d440505e2c1281aa6
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-03-26 18:23:51 +0300

    process.env

--- src/nats/conn.ts
+++ src/nats/conn.ts
@@ -1,11 +1,11 @@
 import {connect, StringCodec} from "nats";
 
+export const sc = StringCodec();
+
 export const nats_client = connect({
-    servers: "188.120.227.233:4222",
-    token: "s3cr3t",
+    servers: `${process.env.NATS_SERVER}:${process.env.NATS_PORT}`,
+    token: process.env.NATS_TOKEN,
     debug: false,
     verbose: true,
     maxPingOut: 5
 });
-
-export const sc = StringCodec();
