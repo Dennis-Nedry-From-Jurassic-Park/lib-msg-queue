075304a8f97e235b9ed353b7e9c7c1eb397e60eb
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-04-18 10:55:49 +0300

    right close done

--- src/nats/utils.ts
+++ src/nats/utils.ts
@@ -4,4 +4,4 @@ export const purge = async (stream: string) => {
     const  js = (await nats_client).jetstream();
     const jsm = await (await nats_client).jetstreamManager();
     await jsm.streams.purge(stream);
-}
+} // TODO: right close done https://github.com/nats-io/nats.js/issues/424
